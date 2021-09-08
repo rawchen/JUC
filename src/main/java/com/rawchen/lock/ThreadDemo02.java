816145a8f6b67d964409d57a42dc97b4b5f75ab1
RawChen <2221999792@qq.com>
2021-09-08 22:14:47 +0800

    Lock解决虚假唤醒

---
+++ src/main/java/com/rawchen/lock/ThreadDemo02.java
@@ -0,0 +1,94 @@
+package com.rawchen.lock;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author RawChen
+ * @date 2021/9/8 22:05
+ */
+public class ThreadDemo02 {
+	public static void main(String[] args) {
+		Share share = new Share();
+
+		new Thread(() -> {
+			for (int i = 0; i < 10; i++) {
+				try {
+					share.incr();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}, "AA").start();
+
+		new Thread(() -> {
+			for (int i = 0; i < 10; i++) {
+				try {
+					share.decr();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}, "BB").start();
+
+		new Thread(() -> {
+			for (int i = 0; i < 10; i++) {
+				try {
+					share.incr();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}, "CC").start();
+
+		new Thread(() -> {
+			for (int i = 0; i < 10; i++) {
+				try {
+					share.decr();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}, "DD").start();
+	}
+}
+
+class Share {
+	private int number = 0;
+
+	private Lock lock = new ReentrantLock();
+	private Condition condition = lock.newCondition();
+
+	public void incr() throws InterruptedException {
+		lock.lock();
+		try {
+			while (number != 0) {
+				condition.await();
+			}
+			number++;
+			System.out.println(Thread.currentThread().getName() + " :: " + number);
+			condition.signalAll();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public void decr() throws InterruptedException {
+		lock.lock();
+		try {
+			while (number != 0) {
+				condition.await();
+			}
+			number--;
+			System.out.println(Thread.currentThread().getName() + " :: " + number);
+			condition.signalAll();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			lock.unlock();
+		}
+	}
+}
